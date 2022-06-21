f57e8c401528cb4ddfc51f3c0a849cac65b0e40c
Thiago <thiagoalves060@gmail.com>
2022-06-21 20:56:30 -0300

    [FEAT] projeto web spring

---
+++ src/main/java/com/thiago/crudwebservice/CrudwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.crudwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudwebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/crudwebservice/CrudwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.crudwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
